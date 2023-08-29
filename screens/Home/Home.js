aa6666d10f14c5b6f2d21e4a0d122d0d600b247a
nolanryan <nolan.dev@icloud.com>
2023-08-30 03:17:09 +1000

    finished working button component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -3,13 +3,19 @@ import {SafeAreaView} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Azzahri A.'} type={1} />
-      <Header title={'Azzahri A.'} type={2} />
-      <Header title={'Azzahri A.'} type={3} />
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('hello');
+        }}
+      />
+      <Button title={'Donate'} isDisabled={true} />
     </SafeAreaView>
   );
 };
